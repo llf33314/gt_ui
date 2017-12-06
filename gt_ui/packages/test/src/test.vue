7143d3be54dae9cc405a0d6d728cb15f2a480659
lixiaoqiang <gzhidao1010@gmail.com>
2017-12-06 16:56:02 +0800

    gt-ui test

---
+++ gt_ui/packages/test/src/test.vue
@@ -0,0 +1,16 @@
+<style scoped>
+.gt-test{
+  color: red;
+}
+</style>
+<template>
+  <div>
+    <h1 class="gt-test">i am packages test</h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'gt-test'
+  }
+
+</script>
